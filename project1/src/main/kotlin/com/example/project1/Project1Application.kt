d2b5b3f21f249053e0e29df377a2e59c6c03a4f0
forest6511 <20209757+forest6511@users.noreply.github.com>
2021-06-16 04:22:57 +0900

    Change test.yml

--- project1/src/main/kotlin/com/example/project1/Project1Application.kt
+++ project1/src/main/kotlin/com/example/project1/Project1Application.kt
@@ -3,10 +3,8 @@ package com.example.project1
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @EntityScan("com.example.common.domain")
-@EnableJpaRepositories("com.example.project1.repository")
 @SpringBootApplication
 class Project1Application
 
--- project1/src/main/kotlin/com/example/project1/repository/CategoryRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.project1.repository
-
-import com.example.common.domain.CategoryEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface CategoryRepository: JpaRepository<CategoryEntity, Long> {
-}
